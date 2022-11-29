f0027b9b48d41f02fff4ec706d31d5658da1026a
LionelVanS <lionelvs@outlook.fr>
2022-11-29 19:55:14 +0100

    Adding theme provider, and components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+
+// Components
+import Header from './Components/Header/Header';
+import Main from './Components/Body/Main';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   // Couleurs du thème
+   const theme = createTheme({
+      palette: {
+         primary: {
+            main: '#c4e538',
+         },
+         secondary: {
+            main: '#ff7c22',
+         },
+         tertiary: {
+            main: '#4e5166',
+         },
+         white: {
+            main: '#ffffff',
+         },
+      },
+   });
+
+   return (
+      <ThemeProvider theme={theme}>
+         <BrowserRouter>
+            <Header />
+            <Main />
+         </BrowserRouter>
+      </ThemeProvider>
+   );
 }
 
 export default App;
